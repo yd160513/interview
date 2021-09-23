fd3b43cdf47abe9087ef3936aff622bed564bb38
yd160513 <1223519172@qq.com>
2021-09-23 11:31:23 +0800

    fix: 原来的 bind 返回的函数 new 出来的实例的原型没有指向构造函数,增加完整版

--- bind.js
+++ bind.js
@@ -17,6 +17,7 @@ Function.prototype.myApply = function (context) {
   return result
 }
 
+// new 出来的实例的原型没有指向构造类
 // fn.bind(obj, 1, 2, 3, 4, ...)
 // fn.bind(obj, 1, 2, 3, ...)(123)
 Function.prototype.myBind = function (context) {
@@ -44,6 +45,9 @@ Function.prototype.myBind = function (context) {
   return F
 }
 
+// 完整版
+// fn.bind(obj, 1, 2, 3, 4, ...)
+// fn.bind(obj, 1, 2, 3, ...)(123)
 Function.prototype.myBind2 = function (context, ...outerArgs) {
   // 第一版: 不支持 new
   // return (...innerArgs) => {
@@ -51,16 +55,31 @@ Function.prototype.myBind2 = function (context, ...outerArgs) {
   // }
   // 第二版
   // 缓存调用 bind 的函数
-  const _selfFun = this;
-  // 定义 bind 返回的函数
-  const Fun = function (...innerArgs) {
-    // _selfFun instanceof Fun: 这里的这个判断相当于 new 的是调用 bind 的函数的函数
-    // _selfFun instanceof Fun: 外侧 new 的是 Fun，在 Fun 内部为什么要判断 _self 是不是 Fun 的实例呢
-    if (_selfFun instanceof Fun) return new _selfFun(...outerArgs, ...innerArgs)
-    return _selfFun.call(context, ...outerArgs.concat(...innerArgs))
-  }
-  Fun.prototype = Object.create(_selfFun.prototype)
-  return Fun
+  const thatFunc = this
+  // fBound 是被返回的函数
+  const fBound = function (...innerArgs) {
+    // bind() 方法返回的函数的结果，是调用 bind() 方法的函数的执行结果。
+    // 所以这里采用了外侧的 this，也就是谁调用了 bind() 方法，这里就执行谁。
+    return thatFunc.call(
+      /**
+       * 不论是 new 的方式调用，还是直接调用，它们都会被调用，
+       * 只不过 this 的指向不一样，所以这里需要区分是哪种方式调用。
+       * 因为如果 bind 返回的 fBound 函数是通过 new 来调用的话，则 fBound 的实例的原型必定是指向调用 bind 方法的函数的。也就是说 this 指向必定是指向调用 bind 的函数的。
+       * 所以这里可以通过 instanceof 判断实例的原型是否存在于 thatFunc
+       * 问题: this instanceof thatFunc 中 this 的指向是指向 fBound 函数的，和 thatFunc 有什么关系？
+       *    这里正常是没有关系的，所以需要手动将 fBound 的原型指向给 thatFunc 的原型。
+       *    这样 fBound 的实例就会指向 thatFunc 的原型，this instanceof thatFunc 也就可以成立。
+       */
+      this instanceof thatFunc ? this : context, ...outerArgs, ...innerArgs
+    )
+  } 
+  // 使用 Object.create(thatFunc.prototype) 的原因:
+  //    eg: let YPoint3 = Point.myBind2(null, 1)
+  //        YPoint3.prototype.getName = () => {}
+  //        这个时候就会在 thatFunc.prototype 上增加一个 getName 方法
+  //        所以为了不污染 thatFunc.prototype 所以使用 Object.create() 创建了一个副本
+  fBound.prototype = Object.create(thatFunc.prototype)
+  return fBound
 }
 
 // function sum(...args) {
@@ -95,21 +114,23 @@ let YPoint3 = Point.bind(null, 1)
 let axiosPoint3 = new YPoint3(2)
 console.log(axiosPoint3.toString()) // 1,2
 console.log(axiosPoint3 instanceof Point) // true
+console.log(YPoint3 instanceof Point) // false
 console.log(axiosPoint3 instanceof YPoint3) // true
 
-console.log('myBind: ----------------')
+// console.log('myBind: ----------------')
 // let YPoint = Point.bind(null, 1)
-let YPoint = Point.myBind(null, 1)
-let axiosPoint1 = new YPoint(2)
-console.log(axiosPoint1.toString()) // 1,2
-console.log(axiosPoint1 instanceof Point) // true
-console.log(YPoint instanceof Point) // true
-console.log(axiosPoint1 instanceof YPoint) // false
+// let YPoint = Point.myBind(null, 1)
+// let axiosPoint1 = new YPoint(2)
+// console.log(axiosPoint1.toString()) // 1,2
+// console.log(axiosPoint1 instanceof Point) // true
+// console.log(YPoint instanceof Point) // true
+// console.log(axiosPoint1 instanceof YPoint) // false
 
 console.log('myBind2: ----------------')
 // let YPoint = Point.bind(null, 1)
 let YPoint2 = Point.myBind2(null, 1)
 let axiosPoint2 = new YPoint2(2)
-console.log(axiosPoint2.toString()) // [object Object]
+console.log(axiosPoint2.toString())
 console.log(axiosPoint2 instanceof Point) // true
+console.log(YPoint2 instanceof Point) // false
 console.log(axiosPoint2 instanceof YPoint2) // true
