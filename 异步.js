993594a86363f91db1f0ec98cf2c72c11fc048f3
yd160513 <1223519172@qq.com>
2021-09-27 11:18:32 +0800

    feat: 增加 event loop 的考点

--- 异步.js
+++ 异步.js
@@ -104,3 +104,57 @@ new Promise(resolve => {
   })
 
 console.log('script end') // 4
+
+// ------------------------------------------------------------------------------------------
+/**
+ * 第一题: L1 L2 M1 M2. 
+ * 代码执行循序:
+ * 当用户触发点击事件之后
+ * 将 click1 上下文放到调用栈，调用栈开始执行代码。遇到 Promise.then() 则将其放到任务队列中，执行 console.log('L1') 打印 L1。
+ * L1 执行完成之后，click1 上下文执行完毕，退出调用栈。
+ * 调用栈此时为空，开始读取任务队列中回调，开始执行 console.log('M1') 打印 M1。
+ * 执行完 M1 之后开始进入 click2 的上下文，将其上下文放到调用栈，将 Promise.then() 则将其放到任务队列中，执行 console.log('L2') 打印 L2。
+ * L2 执行完成之后 click2 上下文执行完毕，退出调用栈。
+ * 调用栈此时为空，开始读取任务队列中的会调用，开始执行 console.log('M2') 打印 M2。
+ */
+const btn = document.getElementById('btn')
+// 命名为 click1
+btn.addEventListener('click', () => {
+  Promise.resolve().then(() => { console.log('M1') })
+  console.log('L1')
+})
+// 命名为 click2
+btn.addEventListener('click', () => {
+  Promise.resolve().then(() => { console.log('M2') })
+  console.log('L2')
+})
+btn.click()
+
+/**
+ * 第一题的改版: L1 M1 L2 M2.
+ * 代码执行顺序:
+ * 将 btn.click() 的上下文放到调用栈中。因为是 JS 层面上的代码。1 中没有将点击事件放到调用栈中是因为它是在渲染线程中的。
+ * 将 click1 上下文放到调用栈，调用栈开始执行代码。遇到 Promise.then() 则将其放到任务队列中，执行 console.log('L1') 打印 L1。
+ * L1 执行完成之后，click1 上下文执行完毕，退出调用栈。
+ * 此时调用栈不为空，继续执行代码，不会去任务队列中找。
+ * 将 click2 上下文放到调用栈，调用栈开始执行代码。遇到 Promise.then() 则将其放到任务队列中，执行 console.log('L2') 打印 L2。
+ * L2 执行完成之后，click2 上下文执行完毕，退出调用栈。
+ * 然后将 btn.click() 的上下文从调用栈中退出。
+ * 开始去任务队列中找，因为是队列，所以先执行 M1 再执行 M2。
+ */
+const btn = document.getElementById('btn')
+// 命名为 click1
+btn.addEventListener('click', () => {
+  Promise.resolve().then(() => { console.log('M1') })
+  console.log('L1')
+})
+// 命名为 click2
+btn.addEventListener('click', () => {
+  Promise.resolve().then(() => { console.log('M2') })
+  console.log('L2')
+})
+// 总结: 1 和 2 中的考点就在于时候会去任务队列中找代码去执行？只有在调用栈为空的时候才会去任务队列中找。
+
+// ------------------------------------------------------------------------------------------
+
+
